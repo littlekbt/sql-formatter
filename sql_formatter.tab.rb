fe46b0e2e2b2723d163e3e8795e19be0866e6591
yukikubota <kubota.yuki@adways.net>
2016-12-16 19:38:01 +0900

    version3.1 fromの後にwhereがこない時に一文字しかキャプチャしないバグ修正

--- sql_formatter.tab.rb
+++ sql_formatter.tab.rb
@@ -23,21 +23,26 @@ module_eval(<<'...end sql_formatter.y/module_eval...', 'sql_formatter.y', 32)
         @q.push [:SELECT, $&]
       when /^\*|^.+(?=\s+FROM)/i
         @q.push [:SELECT_LIST, $&]
-      when /(?=^FROM\s+.+?(\s+(?:WHERE|ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|)^(FROM)\s+(.+?)(?(1)(?=\s+(?:WHERE|ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|)/i # ((cond)truepat|falsepat) condには、後方参照の数字を入れる。
+      when /(?=^FROM\s+.+?(\s+(?:WHERE|ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|)^(FROM)\s+(?(1)(.+?)(?=\s+(?:WHERE|ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|(.+))/i # ((cond)truepat|falsepat) condには、後方参照の数字を入れる。
+        cond = $3.nil? ? $4 : $3
         @q.push [:FROM,           $2]
-        @q.push [:FROM_CONDITION, $3]
-      when /(?=^WHERE\s+.+?(\s+(?:ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|)^(WHERE)\s+(.+?)(?(1)(?=\s+(?:ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|)/i
+        @q.push [:FROM_CONDITION, cond]
+      when /(?=^WHERE\s+.+?(\s+(?:ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|)^(WHERE)\s+(?(1)(.+?)(?=\s+(?:ORDER\sBY|HAVING|GROUP\sBY|LIMIT))|(.+))/i
+        cond = $3.nil? ? $4 : $3
         @q.push [:WHERE,           $2]
-        @q.push [:WHERE_CONDITION, $3]
-      when /(?=^ORDER\sBY\s+.+?(\s+(?:HAVING|GROUP\sBY|LIMIT))|)^(ORDER\sBY)\s+(.+?)(?(1)(?=\s+(?:HAVING|GROUP\sBY|LIMIT))|)/i
+        @q.push [:WHERE_CONDITION, cond]
+      when /(?=^ORDER\sBY\s+.+?(\s+(?:HAVING|GROUP\sBY|LIMIT))|)^(ORDER\sBY)\s+(?(1)(.+?)(?=\s+(?:HAVING|GROUP\sBY|LIMIT))|(.+))/i
+        cond = $3.nil? ? $4 : $3
         @q.push [:ORDER_BY,           $2]
-        @q.push [:ORDER_BY_CONDITION, $3]
-      when /(?=^HAVING\s+.+?(\s+(?:GROUP\sBY|LIMIT))|)^(HAVING)\s+(.+?)(?(1)(?=\s+(?:GROUP\sBY|LIMIT))|)/i
+        @q.push [:ORDER_BY_CONDITION, cond]
+      when /(?=^HAVING\s+.+?(\s+(?:GROUP\sBY|LIMIT))|)^(HAVING)\s+(?(1)(.+?)(?=\s+(?:GROUP\sBY|LIMIT))|(.+))/i
+        cond = $3.nil? ? $4 : $3
         @q.push [:HAVING,           $2]
-        @q.push [:HAVING_CONDITION, $3]
-      when /(?=^GROUP\sBY\s+.+?(\s+(?:LIMIT))|)^(GROUP\sBY)\s+(.+?)(?(1)(?=\s+(?:LIMIT))|)/i
+        @q.push [:HAVING_CONDITION, cond]
+      when /(?=^GROUP\sBY\s+.+?(\s+(?:LIMIT))|)^(GROUP\sBY)\s+(?(1)(.+?)(?=\s+(?:LIMIT))|(.+))/i
+        cond = $3.nil? ? $4 : $3
         @q.push [:GROUP_BY,           $2]
-        @q.push [:GROUP_BY_CONDITION, $3]
+        @q.push [:GROUP_BY_CONDITION, cond]
       when /^(LIMIT)\s+(.+)/i
         @q.push [:LIMIT,           $1]
         @q.push [:LIMIT_CONDITION, $2]
